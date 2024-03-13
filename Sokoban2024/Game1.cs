bff5cadcdd9ae4e31e1bf603c91143f4b0caa95f
DNogueiraIPCA <dnogueira@ipca.pt>
2024-03-13 12:40:54 +0000

    Adicionado sprites

--- Sokoban2024/Game1.cs
+++ Sokoban2024/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace Sokoban2024
 {
@@ -9,6 +10,13 @@ namespace Sokoban2024
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private SpriteFont font;
+        private int nrLinhas = 0;
+        private int nrColunas = 0;
+        private char[,] level;
+        private Texture2D player, dot, box, wall; //Load images Texture 
+        int tileSize = 64; //potencias de 2 (operações binárias)
+
+
 
 
         public Game1()
@@ -21,6 +29,10 @@ namespace Sokoban2024
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            LoadLevel("level1.txt");
+            _graphics.PreferredBackBufferHeight = tileSize * level.GetLength(1); //definição da altura
+            _graphics.PreferredBackBufferWidth = tileSize * level.GetLength(0); //definição da largura
+            _graphics.ApplyChanges(); //aplica a atualização da janela
 
             base.Initialize();
         }
@@ -29,7 +41,10 @@ namespace Sokoban2024
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("File"); //Use the name of sprite font file ('File')
-            
+            player = Content.Load<Texture2D>("Character4");
+            dot = Content.Load<Texture2D>("EndPoint_Blue");
+            box = Content.Load<Texture2D>("Crate_Brown");
+            wall = Content.Load<Texture2D>("Wall_Brown");
 
             // TODO: use this.Content to load your game content here
         }
@@ -51,12 +66,58 @@ namespace Sokoban2024
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 10), Color.Black);
+            //_spriteBatch.DrawString(font, "O texto que quiser", new Vector2(0, 0), Color.Black);
+            //_spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas} -- Numero de Colunas = {nrColunas}", new Vector2(300, 10), Color.Black);
             
+            Rectangle position = new Rectangle(0, 0, tileSize, tileSize); //calculo do retangulo a depender do tileSize
+            for (int x = 0; x < level.GetLength(0); x++)  //pega a primeira dimensão
+            {
+                for (int y = 0; y < level.GetLength(1); y++) //pega a segunda dimensão
+                {
+                    position.X = x * tileSize; // define o position
+                    position.Y = y * tileSize; // define o position
+
+                    switch (level[x, y])
+                    {
+                        case 'Y':
+                            _spriteBatch.Draw(player, position, Color.White);
+                            break;
+                        case '#':
+                            _spriteBatch.Draw(box, position, Color.White);
+                            break;
+                        case '.':
+                            _spriteBatch.Draw(dot, position, Color.White);
+                            break;
+                        case 'X':
+                            _spriteBatch.Draw(wall, position, Color.White);
+                            break;
+                    }
+                }
+            }
+
             _spriteBatch.End();
 
 
             base.Draw(gameTime);
         }
+
+        void LoadLevel(string levelFile)
+        {
+            string[] linhas = File.ReadAllLines($"Content/{levelFile}");  // "Content/" + level
+            nrLinhas = linhas.Length;
+            nrColunas = linhas[0].Length;
+            
+            level = new char[nrColunas, nrLinhas];
+
+            for (int x = 0; x < nrColunas; x++)
+            {
+                for (int y = 0; y < nrLinhas; y++)
+                {
+                    level[x, y] = linhas[y][x];
+                }
+            }
+
+        }
+
     }
 }
